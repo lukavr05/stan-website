02f2dd415e657d51866dd7243956b322f5c06781
lukavr05 <lvrooyen@yahoo.com>
2025-07-06 21:34:31 +0100

    fixed issues and added routing

---
+++ src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import { Box, Typography } from "@mui/material";
+
+function Home() {
+  return (
+    <>
+      <Box
+        sx={{
+          paddingTop: 8,
+          marginLeft: 10,
+          marginRight: 10,
+        }}
+      >
+        <Typography variant="h1">
+          Sample
+        </Typography>
+
+        <Typography variant="body1">
+          This is some body text to show off the font that will be used for random text
+        </Typography>
+      </Box>
+    </>
+  )
+}
+
+export default Home
--- src/theme.jsx
+++ src/theme.jsx
@@ -1,5 +1,6 @@
 import { createTheme } from '@mui/material/styles';
 import '@fontsource/cutive'
+import '@fontsource/cutive-mono'
 
 const theme = createTheme({
   palette: {
@@ -22,6 +23,11 @@ const theme = createTheme({
   typography: {
     fontFamily: '"Cutive", serif',
     fontWeight: 400,
+
+    body1: {
+      fontFamily: '"Cutive Mono", monospace',
+      fontSize: 20,
+    }
   },
 });
 export default theme;
